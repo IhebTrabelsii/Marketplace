044a0f4a89205a2ea1bee66e96b3935cf9cc6394
Iheb Trabelsi <iheb.trabelsi.gf@gmail.com>
2026-05-04 15:15:14 +0100

    backend ready for deploy

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.json());
 
 // Database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123$$ihebA',
-    database: 'machine',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: 3306
 });
 
